19438bc3c200e2f00d66996cd59cc33e5bcade42
tan-incedo <140100905+tan-incedo@users.noreply.github.com>
2023-08-02 13:18:40 -0700

    Update NB-StorageAccountGen1.py

--- NB-StorageAccountGen1.py
+++ NB-StorageAccountGen1.py
@@ -3,7 +3,7 @@ storage_account="sataniyav1"
 container_name="v1-container-2aug-taniya"
 source_url="wasbs://{0}@{1}.blob.core.windows.net".format(container_name, storage_account)
 
-access_key="/tZ7exkUHusRPCPzs6D06J3WbljYfiYU07Q4gu3qVwXStMr/TCn+g6KboxtN36TDhputFc3rdZ81+ASt/Z6K6w=="
+access_key=""
 mount_point_url="/mnt/gen1dataset"
 
 extra_configs_key=f"fs.azure.account.key.{storage_account}.blob.core.windows.net"
