2d4511a4d476ce0bd10014bde44b6dd5f94fb82e
tan-incedo <tan-incedo>
2023-08-04 19:22:31 +0000

    s3 to ADB

--- Notebook-4aug.py
+++ Notebook-4aug.py
@@ -24,3 +24,40 @@ dbutils.fs.ls("s3://bkt-taniya-4aug/input/iris.csv")
 
 df=spark.sql("SELECT * FROM parquet.`s3://bkt-taniya-4aug/input/userdata1.parquet`")
 df.head()
+
+# COMMAND ----------
+
+
+access_key = ""
+
+secret_key = ""
+
+ 
+
+#Mount bucket on databricks
+
+encoded_secret_key = secret_key.replace("/", "%2F")
+
+aws_bucket_name = "bkt-taniya-4aug"
+
+mount_name = "taniyaawss3"
+
+dbutils.fs.mount("s3a://%s:%s@%s" % (access_key, encoded_secret_key, aws_bucket_name), "/mnt/%s" % mount_name)
+
+display(dbutils.fs.ls("/mnt/%s" % mount_name))
+
+
+mount_name = "taniyaawss3"
+
+file_name="iris.csv"
+
+df = spark.read.format("csv").load("/mnt/%s/%s" % (mount_name , file_name))
+
+
+
+
+# COMMAND ----------
+
+df.show()
+strMountPointParquetFile="/mnt/%s/abc.parquet" % (mount_name)
+df.write.parquet(strMountPointParquetFile)
